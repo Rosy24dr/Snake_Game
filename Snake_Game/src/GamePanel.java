0deeae39d8a51f51685947533755336584b5d3bf
Rosy24dr <rosy24dr@outlook.com>
2022-07-12 23:45:10 -0400

    finished the GamePanel Constructor.

--- Snake_Game/src/GamePanel.java
+++ Snake_Game/src/GamePanel.java
@@ -6,8 +6,32 @@ import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
-
+	
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600; 
+	static final int UNIT_SIZE = 25; 
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] =new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten;
+	int appleX; 
+	int apppleY; 
+	char direction = 'R';
+	boolean running = false; 
+	Timer timer;
+	Random random;
+	
+	
 	GamePanel(){
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+		
 		
 	}
 	
@@ -20,6 +44,10 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	
 	public void draw(Graphics g) {
+		
+	}
+	
+	public void newApple() {
 		
 	}
 	
