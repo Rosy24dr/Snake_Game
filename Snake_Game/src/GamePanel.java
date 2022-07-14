e0e591070fb800368dc245c783831f16c01d0983
Rosy24dr <rosy24dr@outlook.com>
2022-07-13 23:41:23 -0400

    Finished the checkCollision, move actionPerformed methods and MyKeyAdapter class

--- Snake_Game/src/GamePanel.java
+++ Snake_Game/src/GamePanel.java
@@ -104,7 +104,32 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	
 	public void checkCollisions() {
+		for(int i = bodyParts;i>0;i--) {
+			if(x[0] == x[i]&& (y[0] == y[i])) {
+				running = false;
+			}
+		}
+		if(x[0] < 0) {
+			running = false;
+		}
 		
+		if(x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		
+		if(y[0] < 0) {
+			running = false;
+		}
+		
+		if(y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+		
+		if(!running) {
+			timer.stop();
+		}
+			
+				
 	}
 	
 	public void gameOver(Graphics g) {
@@ -114,12 +139,43 @@ public class GamePanel extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		if (running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
+		
 		
 	}
 	
 	public class MyKeyAdapter extends KeyAdapter{
 		@Override
 		public void keyPressed(KeyEvent e) {
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R') {
+					direction = 'L';	
+				}
+				break;
+				
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L') {
+					direction = 'R';	
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';	
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U') {
+					direction = 'D';	
+				}
+				break;
+			
+			}
 			
 		}
 	}
